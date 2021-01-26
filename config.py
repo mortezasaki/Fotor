fd38bc0f5a40d43ca9c480c0db0517e01ea08762
Morteza <mortezasaki91@gmail.com>
2021-01-26 13:44:37 +0330

    Add app configs

--- config.py
+++ config.py
@@ -4,4 +4,8 @@ Config = {
     "account_path" : "%s/Accounts/" % os.getcwd(),
     "tg_api_id" : 21724,
     "tg_api_hash" : "3e0cb5efcd52300aec5994fdfc5bdc16",
+    "device_model" : "Galaxy J5 Prime",
+    "system_version" : "SM-G570F",
+    "app_version" : "1.0.0",
+    "flood_sleep_threshold" : 60 * 60 * 12,
 }
