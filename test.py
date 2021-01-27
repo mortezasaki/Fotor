8dddb4b156f3e118eb43f5e0579c93faaab9333e
Morteza <mortezasaki91@gmail.com>
2021-01-27 09:58:18 +0330

    test_valid_username

--- test.py
+++ test.py
@@ -11,10 +11,18 @@ class TestUtility(unittest.TestCase):
         self.assertFalse(utility.ValidatePhone('98916test8'))
 
 class TestJoin(unittest.TestCase):
-    async def test_login(self):
-        _join = Join('639657163753')
-        self.assertTrue(await _join.Login())
+    def __init__(self, *args, **kwargs):
+        super(TestJoin, self).__init__(*args, **kwargs)
+        self.join = Join('639657163753')
 
+    async def test_login(self):
+        self.assertTrue(await self.join.Login())
+
+    def test_valid_username(self):
+        self.assertTrue(self.join.ValidUsername('Morteza'))
+        self.assertTrue(self.join.ValidUsername('Mor43443'))
+        self.assertFalse(self.join.ValidUsername('1234344'))
+        self.assertFalse(self.join.ValidUsername('dsfd'))
 
 if __name__ == '__main__':
     unittest.main()
