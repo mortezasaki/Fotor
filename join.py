900403ad203e2e0979c4e420e2cf40ad31057c4b
Morteza <mortezasaki91@gmail.com>
2021-01-29 20:31:05 +0330

    Fix bugs

--- join.py
+++ join.py
@@ -23,6 +23,7 @@ from api import API
 from enums import *
 import requests
 
+
 logging.getLogger().setLevel(logging.INFO)
 
 
@@ -88,20 +89,22 @@ class SMSActivate:
             return sorted_costs
         return None
 
-    def GetNumber(self, country_code, service= 'tg'):
+    def GetNumber(self, country_code, service= 'tg', retry : int = 5, wait : float = 3):
         url = 'https://sms-activate.ru/stubs/handler_api.php?api_key={0}&action=getNumber&service={1}&country={2}'.format(self.api_key, service, country_code)
-        req = requests.get(url)        
+        for i in range(retry):
+            req = requests.get(url)        
 
-        if req.status_code == 200:
-            try:
-                response = req.text
-                pattern = r'^(ACCESS_NUMBER:)\d{8,}(:)\d{8,}$'
-                if re.match(pattern, response):
-                    status_code = response.split(':')[1]
-                    phone_number = response.split(':')[2]
-                    return {'Status' : status_code, 'Phone' : phone_number}
-            except:
-                return None
+            if req.status_code == 200:
+                try:
+                    response = req.text
+                    pattern = r'^(ACCESS_NUMBER:)\d{8,}(:)\d{8,}$'
+                    if re.match(pattern, response):
+                        status_code = response.split(':')[1]
+                        phone_number = response.split(':')[2]
+                        return {'Status' : status_code, 'Phone' : phone_number}
+                except:
+                    return None
+            sleep(wait)
         return None
 
     def ChangeNumberStatus(self,id, status):
@@ -132,43 +135,69 @@ class SMSActivate:
             if req.status_code == 200:
                 pattern = r'^(STATUS_OK:)\d{5}$' # ex STATUS_OK:20980
                 if re.match(pattern, req.text):
-                    print(req.text.split(':')[1])
                     return req.text.split(':')[1]
             sleep(wait)
         return None 
 
-# class SignUp:
-
-
-
-class Join:
-    def __init__(self, phone_number : str):
+class Telegram:
+    def __init__(self, phone_number):
         self.phone_number = phone_number
-        self.client = ''
+        if not os.path.exists(Config['account_path']):
+            os.makedirs(Config['account_path'])
+
+        android_model = utility.AndroidDeviceGenerator()
+        android_model = android_model if android_model is not None else Config['android_model']
+
+        self.tg_session_location = '{0}{1}.session'.format(Config['account_path'],self.phone_number)
+
+        self.tg_client = TelegramClient(self.tg_session_location, Config['tg_api_id'], Config['tg_api_hash'],
+            device_model = android_model , system_version = utility.RandomCharacters(), app_version = utility.RandomCharacters(size = 5),
+            flood_sleep_threshold = Config['flood_sleep_threshold'])
 
     def ValidUsername(self, username : str):
         pattern = r'^[a-zA-Z]\w{5,}$'
         if re.match(pattern, username):
             return True
         return False
+    
+
+    async def SendCode(self):
+        await self.tg_client.connect()
+        
+        await self.tg_client.connect()
+        if self.tg_client.is_connected():
+            try:
+                await self.tg_client.send_code_request(self.phone_number)
+                return True
+            except:
+                return False
+        return False
+
+    async def SignUp(self, activation_code, name, family):
+        try:
+            await self.tg_client.sign_up(activation_code, name, family)
+            return True
+        except:
+            return False
 
     async def Login(self):
-        tg_session_location = '{0}{1}.session'.format(Config['account_path'],self.phone_number)
-        if os.path.exists(tg_session_location):
-            self.client = TelegramClient(tg_session_location, Config['tg_api_id'], Config['tg_api_hash'],
-                device_model = Config['device_model'], system_version = Config['system_version'], app_version = Config['app_version'],
-                flood_sleep_threshold = Config['flood_sleep_threshold'])
-            await self.client.connect()
-            if self.client.is_connected():
-                return True
-            return False
-        else:
-            raise False
+        pass
+        # tg_session_location = '{0}{1}.session'.format(Config['account_path'],self.phone_number)
+        # if os.path.exists(tg_session_location):
+        #     self.client = TelegramClient(tg_session_location, Config['tg_api_id'], Config['tg_api_hash'],
+        #         device_model = ConfSMS_Activate_APIig['device_model'], system_version = Config['system_version'], app_version = Config['app_version'],
+        #         flood_sleep_threshold = Config['flood_sleep_threshold'])
+        #     await self.client.connect()
+        #     if self.client.is_connected():
+        #         return True
+        #     return False
+        # else:
+        #     raise False
 
     async def Search(self, username : str):
         if self.ValidUsername(username):
             try :
-                return await self.client.get_entity(username)
+                return await self.tg_client.get_entity(username)
             except ValueError:
                 return None
             except errors.FloodWaitError as e:
@@ -179,7 +208,7 @@ class Join:
     async def JoinChannel(self, username : str):
         if self.Search(username):
             try :
-                return await self.client(JoinChannelRequest(username))
+                return await self.tg_client(JoinChannelRequest(username))
             except ValueError:  
                 return None
             except errors.FloodWaitError as e:
@@ -189,27 +218,68 @@ class Join:
 
     async def GetChannels(self):
         channels=[]
-        async for dialog in self.client.iter_dialogs():
+        async for dialog in self.tg_client.iter_dialogs():
             if not dialog.is_group and dialog.is_channel:
                 channels.append(dialog)
         print(channels)
         return channels
-    
-
-
 
 if __name__ == "__main__":
-    phone = sys.argv[1]
-    logging.info("Your phone is %s" % phone)
-    _join = Join(phone)
-    logging.info('Join')
-    loop = asyncio.get_event_loop()
-    is_login = loop.run_until_complete(_join.Login())
+    logging.info("Start Fotor...")
 
-    if is_login:
-        logging.info('Login..')
-        _api = API(phone)
-        _api.CallRegisterAPI("Morrr","Saki",Gender.Man.value,'Iran',status =TelegramRegisterStats.Succesfull.value)
+    loop = asyncio.get_event_loop()
+
+    sms_activate = SMSActivate(Config['SMS_Activate_API'])
+    _api = ''
+    telegram = ''
+    balance = sms_activate.Balance()
+    logging.info("Your balance at sms-activate.rus is: %s" % balance)
+    countries = sms_activate.SortCountriesByPrice()
+
+    is_signup = False
+
+    for country_code,cost in countries.items():
+        logging.info('Country {0}, Cost {1}'.format(country_code, cost))
+        if cost <= balance:
+            phone_number = sms_activate.GetNumber(country_code)
+            if phone_number is not None:
+                status = phone_number['Status']
+                phone_number = phone_number['Phone']
+                logging.info('Status: {0}, Phone Number: {1}'.format(status, phone_number))
+                logging.info('Start Telethon...')
+                telegram = Telegram(phone_number)
+                connect_to_telegram = loop.run_until_complete(telegram.SendCode())
+                if connect_to_telegram:
+                    logging.info('The activation code telegram was sent')
+                    logging.info('Wait for activation code...')
+                    try:
+                        activation_code = sms_activate.GetActivationCode(status)
+                        if activation_code is not None:
+                            logging.info('Activation code is: %s' % activation_code)                    
+                            name = utility.FakeNameGenerator()
+                            if name is not None and len(name.split())>1:
+                                family = name.split()[1]
+                                name = name.split()[0]                                
+                            else:
+                                name, family = utility.RandomCharacters(), utility.RandomCharacters()
+                            
+                            logging.info('New user name = {0} {1}'.format(name, family))
+
+                            logging.info('Sign Up in Telegram...')
+                            is_signup = loop.run_until_complete(telegram.SignUp(activation_code, name, family))
+                            if is_signup:
+                                sms_activate.ConfirmCode(status)
+                                _api = API(phone_number)
+                                _api.CallRegisterAPI(name, family ,Gender.Man.value,'Russia',status =TelegramRegisterStats.Succesfull.value)
+                                logging.info('Complate %s sing up' % phone_number)
+                                break
+                    except:
+                        sms_activate.CancelCode(status)
+
+
+    if is_signup:
+        logging.info('Start joining...')
+        
         while True:
             channel = _api.CallGetChannel()
             if channel is not None:
@@ -217,12 +287,14 @@ if __name__ == "__main__":
                 logging.info('Join to %s' % channel_username)
                 channel_id = channel['_id']
                 try:
-                    loop.run_until_complete(_join.Search(channel_username))
-                    loop.run_until_complete(_join.JoinChannel(channel_username))
+                    loop.run_until_complete(telegram.Search(channel_username))
+                    loop.run_until_complete(telegram.JoinChannel(channel_username))
                     if _api.CallJoin(channel_id):
                         logging.info('Join was doned')
                     sleep(1)
                 except errors.UserDeactivatedBanError:
                     logging.info('The user has been banned')
+                except Exception as e:
+                    logging.info(str(e))
 
 
