51cfdb204ebf733c4168ee4cd77fd6eb13fb8e22
Morteza <mortezasaki91@gmail.com>
2021-01-29 14:21:16 +0330

    Add sort

--- utility.py
+++ utility.py
@@ -12,3 +12,8 @@ def ValidatePhone(phone):
     pattern = '^\d{8,}$' # ex: 989161234578
     if re.match(pattern, phone):
         return True
+
+def SortDic(data):
+    if data is not None:
+        return dict(sorted(data.items(), key=lambda item: item[1]))
+    return None
