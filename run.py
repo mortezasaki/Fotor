34e4a96ede83092e0358a6801497762bcb2fbc2c
Morteza <mortezasaki91@gmail.com>
2021-01-30 15:26:50 +0330

    Add exit

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 import cmd
+import ps
 
 class FotorShell(cmd.Cmd):
-    intro = 'Welcome to the fotor shell.'
+    intro = 'Welcome to the Fotor shell.'
     prompt = '(Fotor)'
+
+    def do_new(self, arg):
+        'Create new telegram account'
+        ps.start('python join.py --log debug -v')
+
+    def do_exit(self, arg):
+        'Exit from shell'
+        exit()
+
 
 if __name__ == '__main__':
     FotorShell().cmdloop()
