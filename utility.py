a0e5e6cd83be9ad5ccb1bc30713e105694167d12
Morteza <mortezasaki91@gmail.com>
2021-01-31 23:11:28 +0330

    Add more sites to crawl  proxy

--- utility.py
+++ utility.py
@@ -46,25 +46,67 @@ def AndroidDeviceGenerator():
 def RandomCharacters(size : int = 8, chars = string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def GetProxy(retry : int = 5, wait : float = 5):
-    url = r'https://www.socks-proxy.net/'
-    for i in range(retry):
-        req = requests.get(url)
-        if req.status_code == 200:
-            text = req.text
-            soup = BeautifulSoup(text, 'html.parser')
-            rows = soup.findAll('tr')
-            for row in rows[1:]:
-                if not random.choice([True, False]): # for randomize selction proxy
-                    continue
-                cols = row.findAll('td')
-                if len(cols)>1:
-                    # matching IPv4 Addresss https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
-                    ip_pattern = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
-                    port_pattern = r'\d{4,5}'
-                    ip = cols[0].text
-                    port = cols[1].text
-                    if re.match(ip_pattern, ip) and re.match(port_pattern, port):
-                        return {'IP' : ip, 'Port' : port}
+def GetProxy(retry : int = 5, wait : float = 5, method : int = 2):
+    if method == 1:
+        url = r'http://www.freeproxylists.net/?c=&pt=&pr=HTTPS&a%5B%5D=0&a%5B%5D=1&a%5B%5D=2&u=0'
+        for i in range(retry):
+            req = requests.get(url)
+            if req.status_code == 200:
+                text = req.text
+                soup = BeautifulSoup(text, 'html.parser')                
+                rows = soup.findAll('tr')
+                for row in rows[1:]:
+                    # if not random.choice([True, False]): # for randomize selction proxy
+                    #     continue
+                    cols = row.findAll('td')
+                    if len(cols)>1:
+                        # matching IPv4 Addresss https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
+                        ip_pattern = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
+                        port_pattern = r'\d{4,5}'
+                        ip = cols[0].find('a').text
+                        port = cols[1].text
+                        if re.match(ip_pattern, ip) and re.match(port_pattern, port):
+                            return {'IP' : ip, 'Port' : port}        
+    elif method == 2:
+        url = r'https://www.sslproxies.org/'
+        for i in range(retry):
+            req = requests.get(url)
+            if req.status_code == 200:
+                text = req.text
+                soup = BeautifulSoup(text, 'html.parser')
+                rows = soup.findAll('tr')
+                for row in rows[1:]:
+                    if not random.choice([True, False]): # for randomize selction proxy
+                        continue
+                    cols = row.findAll('td')
+                    if len(cols)>1:
+                        # matching IPv4 Addresss https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
+                        ip_pattern = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
+                        port_pattern = r'\d{4,5}'
+                        ip = cols[0].text
+                        port = cols[1].text
+                        if re.match(ip_pattern, ip) and re.match(port_pattern, port):
+                            return {'IP' : ip, 'Port' : port}
+    elif method == 3:
+        url = r'https://spys.one/en/socks-proxy-list/'
+        for i in range(retry):
+            req = requests.get(url)
+            if req.status_code == 200:
+                text = req.text
+                soup = BeautifulSoup(text, 'html.parser')
+                rows = soup.findAll('tr')
+                for row in rows[1:]:
+                    if not random.choice([True, False]): # for randomize selction proxy
+                        continue
+                    cols = row.findAll('td')
+                    if len(cols)>1:
+                        # matching IPv4 Addresss https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
+                        ip_pattern = r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
+                        port_pattern = r'\d{4,5}'
+                        ip = cols[0].findAll('font')
+                        port = ip[1].text
+                        ip = ip[0].text
+                        if re.match(ip_pattern, ip) and re.match(port_pattern, port):
+                            return {'IP' : ip, 'Port' : port}
     
     return None
