6361d16866993a4e2cfe206a48fb745b2695249c
mortezasaki <mortezasaki91@gmail.com>
2021-02-03 13:39:30 +0330

    Add tail function

--- ps.py
+++ ps.py
@@ -2,6 +2,8 @@
 # For interact with procces like telegram cli when ask a user for input
 
 import subprocess
+import select
+from time import sleep
 
 def start(executable_file):
     return subprocess.Popen(
@@ -16,7 +18,9 @@ def is_running(process):
     return False
 
 def read(process):
-    return process.stdout.readline().decode("utf-8").strip()
+    out = process.stdout
+    if out is not None:
+        return process.stdout.readline().decode("utf-8").strip()
 
 
 def write(process, message):
@@ -28,3 +32,16 @@ def terminate(process):
     process.stdin.close()
     process.terminate()
     process.wait(timeout=0.2)
+
+# https://stackoverflow.com/a/12523371/9850815
+def tail(filename):
+    f = subprocess.Popen(['tail','-F',filename],\
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    
+    p = select.poll()
+    p.register(f.stdout)
+
+    while True:
+        if p.poll(1):
+            print (str(f.stdout.readline()).replace('\n',''))
+        sleep(1)
