02ec3c17bea38a22549b413b16b9ac5aa14e3f33
mortezasaki <mortezasaki91@gmail.com>
2021-02-03 16:14:52 +0330

    Fix cmd KeyboardInterrupt

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import cmd
 import ps
 from time import sleep
 
-
 process_list = []
 class FotorShell(cmd.Cmd):
     intro = 'Welcome to the Fotor shell.'
@@ -10,17 +9,19 @@ class FotorShell(cmd.Cmd):
 
     def do_new(self, arg):
         'Create new telegram account'
-        # new_fotor = ps.start('python join.py --log debug -v')
-        process_list.append(ps.start(['python', 'join.py', '--log' ,'debug', '-v']))
-        print('Prees CTRL+C to exit')
-        sleep(2)
-        while ps.is_running(process_list[-1]):
-            try:
-                process_log = ps.read(process_list[-1])
-                if len(process_log) > 0:
-                    print(process_log)
-            except :
-                break
+        new_fotor = ps.start(['python', 'join.py', '--log' ,'debug', '-v'])
+        process_list.append(new_fotor)
+        # print('Prees CTRL+C to exit')
+        # sleep(1)
+        # while ps.is_running(new_fotor):
+        #     try:
+        #         ps.tail('logs/%s.log' % new_fotor.pid)
+        #         # process_output, _ =  new_fotor.communicate()
+        #         # process_log = ps.read(new_fotor)
+        #         # if process_log is not None:
+        #         #     print(process_log)
+        #     except KeyboardInterrupt:
+        #         break
 
     def do_list(self, arg):
         'List of all running accounts'
@@ -29,6 +30,17 @@ class FotorShell(cmd.Cmd):
             if ps.is_running(process):
                 print(f'{id:<10}', f'{process.pid:<10}')
 
+    def do_log(self, arg):
+        arg = int(arg)
+        while ps.is_running(process_list[arg]):
+            try:
+                ps.tail('logs/%s.log' % process_list[arg].pid)
+                # process_output, _ =  new_fotor.communicate()
+                # process_log = ps.read(new_fotor)
+                # if process_log is not None:
+                #     print(process_log)
+            except KeyboardInterrupt:
+                return True
             
 
 
@@ -39,6 +51,15 @@ class FotorShell(cmd.Cmd):
     # Handle when press enter do nothing https://stackoverflow.com/a/21066546/9850815
     def emptyline(self):
          pass
+
+    def cmdloop(self, intro=None):
+        print(self.intro)
+        while True:
+            try:
+                super(FotorShell, self).cmdloop(intro="")
+                break
+            except KeyboardInterrupt:
+                print("^C")        
 
 
 
