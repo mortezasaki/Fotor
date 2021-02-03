ef488ecc1c00b07c6a3d64ba1404631098fedc0e
mortezasaki <mortezasaki91@gmail.com>
2021-02-03 12:17:50 +0330

    prety print list of process

--- run.py
+++ run.py
@@ -1,28 +1,35 @@
 import cmd
 import ps
 from time import sleep
-import psutil
 
 
-def ProccessExist(pid):
-    return pid in (p.pid for p in psutil.process_iter())
-
+process_list = []
 class FotorShell(cmd.Cmd):
     intro = 'Welcome to the Fotor shell.'
     prompt = 'Fotor: '
 
     def do_new(self, arg):
         'Create new telegram account'
-        new_fotor = ps.start('python join.py --log debug -v')
+        # new_fotor = ps.start('python join.py --log debug -v')
+        process_list.append(ps.start(['python', 'join.py', '--log' ,'debug', '-v']))
         print('Prees CTRL+C to exit')
-        sleep(1)
-        while ps.is_running(new_fotor):
+        sleep(2)
+        while ps.is_running(process_list[-1]):
             try:
-                log = ps.read(new_fotor)
-                if len(log) > 0:
-                    print(log)
+                process_log = ps.read(process_list[-1])
+                if len(process_log) > 0:
+                    print(process_log)
             except :
                 break
+
+    def do_list(self, arg):
+        'List of all running accounts'
+        print(f'{"Index":<10}', f'{"ProcessId":<10}')
+        for id, process in enumerate(process_list):
+            if ps.is_running(process):
+                print(f'{id:<10}', f'{process.pid:<10}')
+
+            
 
 
     def do_exit(self, arg):
