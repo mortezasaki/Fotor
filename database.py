b9717e9612fb978906212ad6843f4dfededa0b8f
mortezasaki <mortezasaki91@gmail.com>
2021-02-05 22:07:54 +0330

    Bug fix

--- database.py
+++ database.py
@@ -7,69 +7,60 @@ class Database:
     def __init__(self):
         self.conn = sqlite3.connect('data.db')
 
-    def ExistDB(self):
-        if not os.path.exists('data.db'):
-            return True
-        return False
-
     def Create(self):
-        if ExistDB():
-            c = self.conn.cursor()
+        c = self.conn.cursor()
 
-            # Create table
-            c.execute('''CREATE TABLE account
-                        (phonenumber text PRIMARY KEY,
-                        country text,
-                        firstname text,
-                        family text,
-                        gender INTEGER,
-                        date_creation text,
-                        status integer)
-                        ''')
-            
-            c.execute('''CREATE TABLE joins
-                        (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        FOREIGN KEY(phonenumber) REFERENCES account(phonenumber),
-                        channel text,
-                        date_join text)
-                        ''')
+        # Create table
+        c.execute('''CREATE TABLE account
+                    (phonenumber text PRIMARY KEY,
+                    country text,
+                    firstname text,
+                    family text,
+                    gender INTEGER,
+                    date_creation text,
+                    status integer)
+                    ''')
+        
+        c.execute('''CREATE TABLE joins
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    phonenumber text,
+                    channel text,
+                    date_join text,
+                    FOREIGN KEY(phonenumber) REFERENCES account(phonenumber))
+                    ''')
 
 
-            # Save (commit) the changes
-            conn.commit()
+        # Save (commit) the changes
+        self.conn.commit()
 
-            # We can also close the connection if we are done with it.
-            # Just be sure any changes have been committed or they will be lost.
-            conn.close()
-            return True
-        return False
+        # We can also close the connection if we are done with it.
+        # Just be sure any changes have been committed or they will be lost.
+        self.conn.close()
     
     def NewAccount(self, phonenumber, country, firstname, family, gender):
-        self.Create()
         if utility.ValidatePhone(phonenumber):
-            c = conn.cursor()
+            c = self.conn.cursor()
             c.execute("INSERT INTO account VALUES ({phonenumber},{country},{firstname},{family},{gender},{datetime.datetime.now()})")
             
             # Save (commit) the changes
-            conn.commit()
+            self.conn.commit()
 
             # We can also close the connection if we are done with it.
             # Just be sure any changes have been committed or they will be lost.
-            conn.close()
+            self.conn.close()
             return True
         return False
 
     def Join(self, phonenumber, channel):
-        self.Create()
         if utility.ValidatePhone(phonenumber):
             c = conn.cursor()
             c.execute("INSERT INTO joins VALUES ({phonenumber},{channel},{datetime.datetime.now()})")
             
             # Save (commit) the changes
-            conn.commit()
+            self.conn.commit()
 
             # We can also close the connection if we are done with it.
             # Just be sure any changes have been committed or they will be lost.
-            conn.close()
+            self.conn.close()
             return True
         return False
