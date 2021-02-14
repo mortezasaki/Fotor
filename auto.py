09b35108a9143a76cc4cc99767e2b22854d22d43
mortezasaki <mortezasaki91@gmail.com>
2021-02-14 11:32:17 +0330

    Add handle ctrl+c intrupt

--- auto.py
+++ auto.py
@@ -8,6 +8,27 @@ import utility, ps
 import random
 from time import sleep
 import datetime
+import logging
+from pytz import timezone, utc
+import signal
+
+def LogInit(phone_number):
+    # output log on stdout https://stackoverflow.com/a/14058475/9850815
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    log_file_name = 'logs/join.log'
+
+    logging.basicConfig(filename=log_file_name, filemode="a", level=logging.INFO,
+        format = '%(asctime)s - {0} - %(message)s'.format(phone_number), datefmt="%Y-%m-%d %H:%M:%S") 
+    
+    logging.Formatter.converter = customTime
+
+# Use custom timezone in logging https://stackoverflow.com/a/45805464/9850815
+def customTime(*args):
+    utc_dt = utc.localize(datetime.utcnow())
+    my_tz = timezone("Asia/Tehran")
+    converted = utc_dt.astimezone(my_tz)
+    return converted.timetuple()    
 
 def GetPhoneFromCMDLine(cmdline):
     pattern = r'\d{10,}'
@@ -34,6 +55,22 @@ def GetJoinProcess():
     
     return process
 
+# for issue 16
+def CheckLimitation():
+    db = Database()
+    limit = Config['account_per_day']
+    accounts = db.GetAccounts()
+    count_of_account_created_today = 0
+
+    for account in accounts:
+        _time = account[5]
+        _time = datetime.datetime.strptime(_time, '%Y-%m-%d %H:%M:%S.%f')
+        if datetime.datetime.today().date() == _time.date():
+            count_of_account_created_today+=1
+    if count_of_account_created_today < limit:
+        return True
+    return False
+
 def GetSignUpProcess():
     process = []
     try:
@@ -59,7 +96,11 @@ def main():
             db = Database()
             try:
                 for file in os.listdir(Config['account_path']):
-                    if file.endswith(".session"):
+                    # fix load account for join when sign up not complated
+                    file_creation = os.path.getctime('%s%s' %(Config['account_path'], file))
+                    file_creation = datetime.datetime.fromtimestamp(file_creation)
+                    diff = (datetime.datetime.now() - file_creation).total_seconds() / 60.0 
+                    if file.endswith(".session") and diff > 2:
                         phone_number = file.split('.')[0]
                         if utility.ValidatePhone(phone_number):
                             status = db.GetStatus(phone_number)
@@ -67,7 +108,7 @@ def main():
                 
                 stoped_accounts = []
                 
-                if accounts is not None:
+                if len(accounts) > 0:
                     for account in accounts:
                         status = account['Status']
                         showed = [TelegramRegisterStats.Ban.value, TelegramRegisterStats.AuthProblem.value,
@@ -100,16 +141,27 @@ def main():
                             sleep(1)
                     else:
                         count_of_signup_process = len(GetSignUpProcess())
-                        if (len(stoped_accounts) + count_of_signup_process) < limit_account:
+                        if (len(stoped_accounts) + count_of_signup_process) < limit_account and CheckLimitation():
                             ps.start(['python', 'telegram_signup.py', '--log' ,'debug', '-v'])
                 else :
-                    ps.start(['python', 'telegram_signup.py', '--log' ,'debug', '-v'])
+                    if CheckLimitation():
+                        ps.start(['python', 'telegram_signup.py', '--log' ,'debug', '-v'])
             except FileNotFoundError:
-                print('No such file or directory')
+                logging.info('No such file or directory')
+            except KeyboardInterrupt: # fix issue 19
+                continue
+            except Exception as e:
+                logging.info(str(e))
 
             db.Close()
 
-
+def handler(signum, frame):
+    print("ctrl+c")
 
 if __name__ == '__main__' :
-    main()
+    try:
+        LogInit('Auto.py')
+        signal.signal(signal.SIGINT, handler)  # prevent "crashing" with ctrl+C https://stackoverflow.com/a/59003480/9850815
+        main()
+    except Exception as e:
+        logging.info(str(e))
