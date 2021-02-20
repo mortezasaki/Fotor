1c3d67b8e5d85c369d1e658f83826c339ef9a8d3
mortezasaki <mortezasaki91@gmail.com>
2021-02-20 20:52:28 +0330

    Fix OS command injection of tail filename #26

--- ps.py
+++ ps.py
@@ -37,7 +37,9 @@ def terminate(process):
     process.wait(timeout=0.2)
 
 # https://stackoverflow.com/a/12523371/9850815
-def tail(filename,searched = None, match = True):
+def tail(filename, searched = None, match = True):
+    if not filename in ('logs/register.log', 'logs/join.log'): # fix security issue for filename
+        return False
     f = subprocess.Popen(['tail','-F',filename],\
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     
